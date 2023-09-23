be95e0377c9d149c5e00b8b85013d6bd8b0c08cb
Minjae <pandalee1147@gmail.com>
2023-09-23 14:29:35 +0900

    change driver opation

--- app/Crawling_app.py
+++ app/Crawling_app.py
@@ -15,7 +15,7 @@ def main(Main_Page_Url):
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
-        options.add_argument("--window-size=1920, 1200")
+        # options.add_argument("--window-size=1920, 1200")
         options.add_argument("--disable-dev-shm-usage")
 
         driver = webdriver.Chrome(options=options)
