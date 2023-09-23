d4596f3c037b05a56e378d9ac9ca119cb5a54fa9
DESKTOP-1VIEEVT\user <pandalee1147@gmail.com>
2023-09-24 01:01:59 +0900

    remove 116~117

--- app/Crawling_app.py
+++ app/Crawling_app.py
@@ -113,8 +113,8 @@ def process(Main_Page_Url:str):
       print("이미 등록된 자료.")
       return "이미 등록된 자료."
 
-if __name__ == '__main__':
-    uvicorn.run("Crawling_app:app", host='0.0.0.0', port=3000, reload=True)
+# if __name__ == '__main__':
+#     uvicorn.run("Crawling_app:app", host='0.0.0.0', port=3000, reload=True)
 
 # # local test용
 # if __name__ == '__main__':
