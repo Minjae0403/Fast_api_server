6c01a4ecd3d1845c3a7e3f383610e6aa5a54e0ed
Minjae <pandalee1147@gmail.com>
2023-09-23 13:51:20 +0900

    update Crawling_app.py webdriver options

--- app/Crawling_app.py
+++ app/Crawling_app.py
@@ -15,6 +15,8 @@ def main(Main_Page_Url):
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920, 1200")
+        options.add_argument("--disable-dev-shm-usage")
 
         driver = webdriver.Chrome(options=options)
         driver.get(Main_Page_Url)
